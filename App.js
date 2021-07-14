cc4456f0e065130b8ead16941a5654e795d517fb
monvanCe <omerfkoca@gmail.com>
2021-07-15 00:45:20 +0300

    v01

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Login from './src/Login'
+
+export default class App extends Component {
+  render() {
+    return (
+    <Login/>
+    )
+  }
+}
---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+const Login = () => 
+ 
+  (
+    <View>
+      <Text> sea lan dayiii </Text>
+    </View>
+  )
+
+
+
+  const Register = () => 
+ 
+  (
+    <View>
+      <Text> ase lan dayiii </Text>
+    </View>
+  )
+
+export default Register
