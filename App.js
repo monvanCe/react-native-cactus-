f55a1813a075b5b06dfdcc326571a08a06f516d9
monvanCe <omerfkoca@gmail.com>
2021-09-23 15:21:36 +0300

    regular

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import Login from './src/Login'
+import Bottomtab from './src/Bottomtab'
 
 export default class App extends Component {
   render() {
     return (
-    <Login/>
+    <Bottomtab/>
     )
   }
 }
---
+++ src/Bottomtabtextstyle.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+ export const styles = StyleSheet.create({
+    bottomText: {
+      color: 'black',
+      fontSize: 80,
+    },
+  });
+
+  export const styles2 = StyleSheet.create({
+    bottomText: {
+      color: 'red',
+      fontSize: 80,
+    },
+  });
