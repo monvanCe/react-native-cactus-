c2f5c5fface8e3aad4cdb354045205a010c9b977
monvanCe <omerfkoca@gmail.com>
2021-09-23 06:13:38 +0300

    bottombarcolor

---
+++ src/Bottomtab.js
@@ -0,0 +1,15 @@
+import { Text, StyleSheet } from "react-native";
+
+ export const styles = StyleSheet.create({
+    bottomText: {
+      color: 'black',
+      fontSize: 80,
+    },
+  });
+
+  export const styles2 = StyleSheet.create({
+    bottomText: {
+      color: 'red',
+      fontSize: 80,
+    },
+  });
